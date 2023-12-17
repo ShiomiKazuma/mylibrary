8713a99c95d04b5d25e9bd537d34d2d2f43da8b5
ShiomiKazuma <kazu_shio_1217@icloud.com>
2023-12-17 20:48:29 +0900

    [add]プレイヤー移動処理を追加

---
+++ Assets/Script/Player/PikminPlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PkuminPlayer : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    [SerializeField] float _rotSpeed;
+    [SerializeField] float _gravSpeed;
+
+    public List<Pikmin> _groupPikmins;
+    [SerializeField] GameObject _pikminPrefab;
+    [SerializeField] Transform _playerGathPos;
+    [SerializeField] Transform _onionPos;
+    [SerializeField] Rigidbody _rb;
+    [SerializeField] Camera camera;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            Pikmin pik = Instantiate(_pikminPrefab, _onionPos.position, Quaternion.identity).GetComponent<Pikmin>();
+        }
+        //ÉvÉåÉCÉÑÅ[ÇÃà⁄ìÆì¸óÕ
+        float x = Input.GetAxis("Horizontal") * _speed;
+        float z = Input.GetAxis("Vertical") * _speed;
+
+        _rb.velocity = (transform.forward * z + Vector3.down * _gravSpeed);
+        transform.Rotate(new Vector3(0, x, 0) * _rotSpeed * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        camera.transform.LookAt(transform.position);
+    }
+}
