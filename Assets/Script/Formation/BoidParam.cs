3ebeeba53d8874e98efeabb6ad3e148168a5c099
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-02-11 23:24:58 +0900

    [add]ボイドアルゴリズムのパラメータのスクリプタブルオブジェクトを追加

---
+++ Assets/Script/Formation/BoidParam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Boid/Param")]
+public class BoidParam : MonoBehaviour
+{
+    [Header("初速")]
+    public float InitSpeed = 2f;
+    [Header("最低速度")]
+    public float MinSpeed = 2f;
+    [Header("最高速度")]
+    public float MaxSpeed = 5f;
+    [Header("隣との距離")]
+    public float NeighborDistance = 1f;
+    [Header("視野角")]
+    public float NeighborFov = 90f;
+    [Header("分離の重み")]
+    public float SeparationWeight = 5f;
+    [Header("壁の大きさ")]
+    public float WallScale = 5f;
+    [Header("壁までの距離感")]
+    public float WallDistance = 3f;
+    [Header("壁の重さ")]
+    public float WallWeight = 1f;
+    [Header("整列の重み")]
+    public float AlignmentWeight = 2f;
+    [Header("結合の重み")]
+    public float CohesionWeight = 3f;
+}
