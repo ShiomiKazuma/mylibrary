bbe3ccda950800de27d0786d5ddb9d923f3b070a
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-02-22 23:06:27 +0900

    [add]プレイヤーの攻撃処理と回避インターフェイスを追加

---
+++ Assets/Script/Player/IAvoidance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAvoidance
+{
+    /// <summary>
+    /// 回避成功処理をする
+    /// </summary>
+    void Avoidance();
+
+    /// <summary>
+    /// ジャスト回避成功処理をする
+    /// </summary>
+    void JustAvoidance();
+
+    /// <summary>
+    /// 回避始めの処理をする
+    /// </summary>
+    void StartAvoidance();
+
+    /// <summary>
+    /// 回避終わりの処理をする
+    /// </summary>
+    void EndAvoidance();
+}
---
+++ Assets/Script/Player/PlayerAttack.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField, Tooltip("攻撃範囲のTrigger")]
+    Collider _attackRange;
+    [SerializeField, Tooltip("敵のHpゲージ")]
+    GameObject _enemyGaze;
+    /// <summary>敵Hpゲージのスライダー </summary>
+    Slider _enemySlider;
+    [SerializeField, Tooltip("当たり判定があるタグ")]
+    string _enemyTagName;
+    [SerializeField, Tooltip("敵に与えるダメージ")]
+    float _damage;
+
+    private void Awake()
+    {
+        //当たり判定を無効にする
+        _attackRange.enabled = false;
+        //敵のゲージを取得
+        _enemySlider = _enemyGaze.GetComponent<Slider>();
+    }
+
+    /// <summary>
+    /// 攻撃を始めるときに呼び出すメソッド
+    /// </summary>
+    /// <param name="damage">与えるダメージ</param>
+    public void AttackStart(float damage)
+    {
+        _damage = damage;
+        _attackRange.enabled = true;
+    }
+
+    /// <summary>
+    /// 攻撃が終わった時に呼び出すメソッド
+    /// </summary>
+    public void AttackEnd()
+    {
+        _attackRange.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //もし、当たったものが敵だったらattack処理をする
+        if(other.gameObject.tag == _enemyTagName)
+        {
+            Attack(_damage);
+        }
+    }
+
+    /// <summary>
+    /// 攻撃処理のメソッド
+    /// </summary>
+    /// <param name="damage">ダメージ</param>
+    void Attack(float damage)
+    {
+        _enemySlider.value -= damage;
+    }
+}
