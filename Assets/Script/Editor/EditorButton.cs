20a17b25b1247dcb94ebfccb9d7e39dd4bd98546
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-02-29 22:21:03 +0900

    [add]ボタン追加のエディタ拡張

---
+++ Assets/Script/Editor/EditorButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+//どのコンポーネントを拡張するのかを指定
+[CustomEditor(typeof(DebugLog))]
+public class EditorButton : Editor
+{
+    //インスペクタを変更する
+    public override void OnInspectorGUI()
+    {
+        //元々あったインスペクタを表示
+        DrawDefaultInspector();
+        
+        //targetは、エディタ拡張上のGameObjectを取得
+        DebugLog debugLog = (DebugLog)target;
+        
+        //ボタンが押されたときの処理を追加
+        if(GUILayout.Button("ボタン"))
+        {
+            //ボタンを押したときに行う処理を記入する
+            debugLog.DebugAction();
+        }
+    }
+}
---
+++ Assets/Script/Test/DebugLog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLog : MonoBehaviour
+{
+    public int PushedCount = 0;
+    /// <summary>
+    /// エディタ拡張でのボタンテスト用
+    /// </summary>
+   public void DebugAction()
+    {
+        PushedCount++;
+        Debug.Log("DebugActionが呼ばれました" + PushedCount);
+    }
+}
