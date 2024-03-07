50faba4901ff052960686ab7c4c0a86bb4112a89
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-03-07 21:20:22 +0900

    [add]コンパスの処理を追加

---
+++ Assets/Script/Map/CompasTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompasTest : MonoBehaviour
+{
+    [SerializeField, Tooltip("コンパスのイメージ")] Image _compasImage;
+    [SerializeField, Tooltip("Player")] Transform _player;
+    [SerializeField, Tooltip("基準の角度")] float _angleOffset = 0f;
+    RectTransform _rt;
+    Quaternion _q;
+    Quaternion _offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rt = _compasImage.GetComponent<RectTransform>();
+        //offsetの初期値を上にする
+        _offset = Quaternion.AngleAxis(_angleOffset, Vector3.up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _q = _player.rotation * _offset;
+        _q.z = _q.y;
+        _q.y = 0f;
+        //そのままだと反対に回るので
+        _q.z = -_q.z;
+        _rt.rotation = _q;
+    }
+}
