db1f2028ac66730e9bf514d3018d2e151c5a5a02
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-03-08 20:40:34 +0900

    [add]汎用的なシングルトンクラスを追加

---
+++ Assets/Script/Singleton/SingletonBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SingletonBase<T> : MonoBehaviour where T : Component
+{
+    static T _instatnce;
+    public static T Instatnce { get { return _instatnce; } }
+
+    /// <summary>
+    /// Awakeで実行したい処理を書く
+    /// </summary>
+    protected abstract void DoAwake();
+
+    protected void Awake()
+    {
+        if(_instatnce == null)
+        {
+            _instatnce = this as T;
+            DontDestroyOnLoad(gameObject);
+            DoAwake();
+        }
+        else
+            Destroy(gameObject);
+    }
+}
