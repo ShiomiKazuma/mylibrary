37646125ce0143e45f7a7b351c242b0567653c60
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-03-10 15:54:02 +0900

    [add]グループ化する機能のエディタ拡張を追加

---
+++ Assets/Script/Editor/GroupEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class GroupEditor : MonoBehaviour
+{
+    [MenuItem("Editor/Group %g")]
+    
+    private static void Grouping()
+    {
+        //選択されているゲームオブジェクトを取得
+        var gameObjects = Selection.gameObjects;
+        //１個も選択されていない場合は、処理をしない
+        if (gameObjects.Length <= 0)
+            return;
+
+        var group = new GameObject("Group");
+        //親オブジェクトを生成
+        Undo.RegisterCreatedObjectUndo(group, "Group");
+        foreach(var obj in gameObjects)
+        {
+            //選択したものを子オブジェクトにする
+            Undo.SetTransformParent(obj.transform, group.transform, "Group");
+        }
+        Selection.activeGameObject = group;
+    }
+
+    [MenuItem("Edit/Group %g", true)]
+    private static bool CanGrouping()
+    {
+        return 0 < Selection.transforms.Length;
+    }
+}
