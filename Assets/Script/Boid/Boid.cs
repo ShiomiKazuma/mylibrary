2db31b3fe08297565439100ad6083d1839806629
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-03-11 23:13:46 +0900

    [add]ボイドアルゴリズムの処理のひな型とステータスデータの追加

---
+++ Assets/Script/Boid/Boid.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public Boidparam Boidparam;
+    public Vector3 Pos { get; private set; }
+    public Vector3 Velocity { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pos = transform.position;
+        Velocity = transform.forward * Boidparam.InitSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 近隣の個体を探して neighbors リストを更新
+        UpdateNeighbors();
+
+        // 壁に当たりそうになったら向きを変える
+        UpdateWalls();
+
+        // 近隣の個体から離れる
+        UpdateSeparation();
+
+        // 近隣の個体と速度を合わせる
+        UpdateAlignment();
+
+        // 近隣の個体の中心に移動する
+        UpdateCohesion();
+
+        // 上記 4 つの結果更新された accel を velocity に反映して位置を動かす
+        UpdateMove();
+    }
+
+    void UpdateNeighbors()
+    {
+
+    }
+
+    void UpdateWalls()
+    {
+
+    }
+
+    void UpdateSeparation()
+    {
+
+    }
+
+    void UpdateAlignment()
+    {
+
+    }
+
+    void UpdateCohesion()
+    {
+
+    }
+
+    void UpdateMove()
+    {
+
+    }
+}
---
+++ Assets/Script/Boid/Boidparam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Boid/Boidparam")]
+public class Boidparam : ScriptableObject
+{
+    public float InitSpeed = 2f;
+    public float MinSpeed = 2f;
+    public float MaxSpeed = 5f;
+    public float NeighborDistance = 1f;
+    public float NeighborFov = 90f;
+    public float SeparationWeight = 5f;
+    public float WallScale = 5f;
+    public float WallDistance = 3f;
+    public float WallWeight = 1f;
+    public float AlignmentWeight = 2f;
+    public float CohesionWeight = 3f;
+}
