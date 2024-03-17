8da65a15d6742f9fedebf5ccc8491dad7b5974fb
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-03-17 20:32:17 +0900

    [add]スタートで遷移する処理

---
+++ Assets/Script/Scene/StartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    [SerializeField] float _changeSceneTime = 1.0f;
+    public void GameStart()
+    {
+        StartCoroutine(GameStartCoroutine());
+    }
+
+    IEnumerator GameStartCoroutine()
+    {
+        yield return new WaitForSeconds(_changeSceneTime);
+        //現在のシーンのインデックス番号を取得
+        int nowSceneNum = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(++nowSceneNum);
+    }
+}
