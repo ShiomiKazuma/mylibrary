ae654271b8da34fc712c843c7b31edc4fb2a0204
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-03-19 23:18:47 +0900

    [add]UIをカメラの正面に設定する処理

---
+++ Assets/Script/UI/UiRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiRotate : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        //向きをカメラの正面に合わせる
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
