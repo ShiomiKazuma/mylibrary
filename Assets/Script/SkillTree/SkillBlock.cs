7aff9ed4a1e8c9e2c8bb388966c57eee34345864
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-03-21 23:40:30 +0900

    [add]スキルツリーの追加

---
+++ Assets/Script/SkillTree/SkillBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBlock : MonoBehaviour
+{
+    SkillType _skillType;
+    [SerializeField, Tooltip("習得に必要なスキル")] SkillType _hasSkill;
+    [SerializeField, Tooltip("コスト")] int _cost;
+    [SerializeField, Tooltip("名前")] string _name;
+    [SerializeField, Tooltip("スキル情報")] string _info;
+
+    public void Onclick()
+    {
+        //　取得済みならなにもしない
+        if (SkillManager.Instatnce.HasSkill(this._skillType))
+        {
+            Debug.Log("習得済み");
+            return;
+        }
+
+        //習得可能かを判定する
+        if (SkillManager.Instatnce.CanLearnSkill(_cost, _hasSkill))
+        {
+            SkillManager.Instatnce.LearnSkill(this._skillType);
+            Debug.Log("習得");
+        }           
+        else
+            Debug.Log("習得不可");
+    }
+}
---
+++ Assets/Script/SkillTree/SkillManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SkillType
+{
+    Attack,
+    Power,
+    Speed,
+    Defend,
+    Null
+}
+
+public class SkillManager : SingletonBase<SkillManager>
+{
+    private int _skillPoint;
+    List<SkillType> _skillList = new List<SkillType>();
+    
+    protected override void DoAwake()
+    {
+        
+    }
+
+    /// <summary>
+    /// スキルが習得済みかを返す
+    /// </summary>
+    /// <param name="skillType">スキル名</param>
+    /// <returns>習得済みか</returns>
+    public bool HasSkill(SkillType skillType)
+    {
+        return _skillList.Contains(skillType);
+    }
+
+    /// <summary>
+    /// スキルが習得できるかを返すメソッド
+    /// </summary>
+    /// <param name="cost">必要なスキルポイント</param>
+    /// <param name="skillType">習得していなければならないスキル</param>
+    /// <returns>スキル習得できたか</returns>
+    public bool CanLearnSkill(int cost, SkillType skillType)
+    {
+        if (cost > _skillPoint)
+            return false;
+
+        //習得すべきスキルがない場合
+        if (skillType == SkillType.Null)
+            return true;
+
+        return HasSkill(skillType);
+    }
+
+    /// <summary>
+    /// スキルを習得するメソッド
+    /// </summary>
+    /// <param name="skillType">習得したスキル</param>
+    public void LearnSkill(SkillType skillType)
+    {
+        _skillList.Add(skillType);
+    }
+}
