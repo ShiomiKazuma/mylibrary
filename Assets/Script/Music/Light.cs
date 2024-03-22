71223b9488b5c5264f39c46534e3f9ef10e5ebb6
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-03-22 21:28:14 +0900

    [add]音ゲーのノーツとライトを作成

---
+++ Assets/Script/Music/Light.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    [SerializeField, Tooltip("スピード")] float _speed = 3f;
+    [SerializeField, Tooltip("")] int _num = 0;
+    Renderer _rendere;
+    float _alfa = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rendere = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!(_rendere.material.color.a <= 0))
+        {
+            _rendere.material.color = new Color(_rendere.material.color.r, _rendere.material.color.g, _rendere.material.color.b, _alfa);
+        }
+
+        if(_num == 1)
+        {
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                colorChange();
+            }   
+        }
+
+        if (_num == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                colorChange();
+            }
+        }
+
+        if (_num == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                colorChange();
+            }
+        }
+
+        if (_num == 4)
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                colorChange();
+            }
+        }
+
+        _alfa -= _speed * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// ボタンが押された時に色を変える
+    /// </summary>
+    void colorChange()
+    {
+        _alfa = 0.3f;
+        _rendere.material.color = new Color(_rendere.material.color.r, _rendere.material.color.g, _rendere.material.color.b, _alfa);
+    }
+}
---
+++ Assets/Script/Music/Notes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    [SerializeField, Tooltip("ノーツの速度")] float _noteSpeed = 5f;
+    private void Update()
+    {
+        transform.position -= transform.forward * Time.deltaTime * _noteSpeed;
+    }
+}
