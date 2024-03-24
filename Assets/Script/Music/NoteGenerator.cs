2fc5460d2ab84655f70d476a58a9b235251ecfaa
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-03-24 22:58:38 +0900

    [add]ノーツの生成処理を追加

---
+++ Assets/Script/Music/NoteGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteGenerator : MonoBehaviour
+{
+    [SerializeField, Tooltip("ノーツのPrefab")] GameObject _notePrefab;
+    [SerializeField, Tooltip("生成場所")] Vector3 _generatoPos;
+
+    public void SpawnNote()
+    {
+        Instantiate(_notePrefab, _generatoPos, Quaternion.identity);
+    }
+}
