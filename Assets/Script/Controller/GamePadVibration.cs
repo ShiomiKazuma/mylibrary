0dc8f972fbb7502eea3ec58cbb349bd21c9292ba
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-04-12 20:20:08 +0900

    [add]ゲームパッドの振動処理を追加

---
+++ Assets/Script/Controller/GamePadVibration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GamePadVibration : MonoBehaviour
+{
+    public void GameVibration(float time)
+    {
+        StartCoroutine(Vibration(time));
+    }
+
+    IEnumerator Vibration(float time)
+    {
+        Gamepad gamepad = Gamepad.current;
+        if(gamepad != null)
+        {
+            //コントローラーを振動させる
+            gamepad.SetMotorSpeeds(1.0f, 1.0f);
+            yield return new WaitForSeconds(time);
+            //コントローラーの振動を止める
+            gamepad.SetMotorSpeeds(0f, 0f);
+        }//ゲームパットなら振動させる
+    }
+}
