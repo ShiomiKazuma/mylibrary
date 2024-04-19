7cf860497fc8d646150a92ff942691c9c70025bb
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-04-19 21:43:34 +0900

    [update]ロックオンセンサーの更新

--- Assets/Script/LockOn/RockonSensor.cs
+++ Assets/Script/LockOn/RockonSensor.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RockonSensor : MonoBehaviour
 {
+    [Tooltip("視点の中心になるオブジェクト")]
+    [SerializeField] Transform _lookTarget;
+    [Tooltip("視野角")]
+    [SerializeField, Range(0, 180)] float _sightAngle;
+    [Tooltip("視界の距離")]
+    [SerializeField] float _sightDistance;
     /// <summary>現在Rockonしているオブジェクト </summary>
     private GameObject _nowTarget;
     /// <summary>感知できる敵のリスト </summary>
@@ -93,5 +100,46 @@ public class RockonSensor : MonoBehaviour
     public List<GameObject> GetEnemyList()
     {
         return _enemyList;
+    }
+
+    public void DefaultRockon()
+    {
+        //近い順に敵をソートする
+        SortEnemyList();
+        for(int i = 0; i < _enemyList.Count; i++)
+        {
+            if (IsVisible(_enemyList[i]))
+            {
+                _nowTarget = _enemyList[i];
+                break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 自分から近い順にリストをソートする
+    /// </summary>
+    public  void SortEnemyList()
+    {
+        List<GameObject> sortedList = _enemyList.OrderBy(e => Vector3.Distance(e.transform.position, transform.position)).ToList();
+        _enemyList.Clear();
+        foreach(var enemy in sortedList)
+        {
+            _enemyList.Add(enemy);
+        }
+    }
+
+    /// <summary>
+    /// 視野角内に敵がいるかを判定
+    /// </summary>
+    /// <returns>視野角内にいるかの判定結果</returns>
+    private bool IsVisible(GameObject target)
+    {
+        Vector3 look = _lookTarget.position - this.transform.position; //視点方向ベクトル
+        Vector3 enmey = target.transform.position - this.transform.position;
+        float cosHalfSight = Mathf.Cos(_sightAngle / 2 * Mathf.Deg2Rad);    // 視野角（の半分）の余弦
+        float cosTarget = Vector3.Dot(look, enmey) / (look.magnitude * enmey.magnitude);  // ターゲットへの角度の余弦
+
+        return cosTarget > cosHalfSight && enmey.magnitude < _sightDistance;
     }
 }
