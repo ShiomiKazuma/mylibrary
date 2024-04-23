72e9a7f7fed5db29f94bbd51b8dfbde580dfaae8
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-04-24 00:18:59 +0900

    [add]残像操作用のコンポーネントを追加

---
+++ Assets/Script/Player/AfterImage/AfterImageControllerBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using Unity.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 残像の操作用コンポーネント
+/// </summary>
+[RequireComponent(typeof(ObservableUpdateTrigger), typeof(ObservableDestroyTrigger))]
+public class AfterImageControllerBase : MonoBehaviour
+{
+    [SerializeField, Header("発生させる残像オブジェクト")] protected AfterImageBase _afterImage = null;
+    [SerializeField, Header("残像の親オブジェクト")] protected Transform _afterImageParent = null;
+    [SerializeField, Header("事前生成数")] protected int _preLoadCount = 5;
+    [SerializeField, Header("残像の生成間隔")] protected float _createIntervalTime = 0.1f;
+    [SerializeField, Header("残像の生存時間")] protected float _afterImageLifeTime = 0.2f;
+    [SerializeField, Header("残像を生成するか")] protected BoolReactiveProperty _isCreate = new BoolReactiveProperty(false);
+    [SerializeReference, Header("発生させる残像オブジェクト"), ReadOnly] protected IAfterImageSetupParam _param = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
