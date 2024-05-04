32b905d0fa5001ea9b352de96b59a213b3640bb3
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-05-05 00:23:42 +0900

    [update]3dでラインレンダラーを生成する処理を作成

--- Assets/Script/Renderer/LineRendererDraw.cs
+++ Assets/Script/Renderer/LineRendererDraw.cs
@@ -5,32 +5,53 @@ using UnityEngine;
 public class LineRendererDraw : MonoBehaviour
 {
     [SerializeField, Tooltip("使用するLineRenderer")] private LineRenderer _lineRenderer;
-    [SerializeField, Tooltip("使用するカメラ")] private Camera _camera;
+    [SerializeField, Tooltip("Rayを飛ばすオブジェクト")] private GameObject _origin;
+    [SerializeField, Tooltip("Rayの長さ")] private float _rayDistance = 100f;
+
     /// <summary>頂点の数 </summary>
     private int _posCount = 0;
     /// <summary>頂点を生成する最低間隔 </summary>
     private float _interval = 0.1f;
+    /// <summary>ボタンを押しているかのフラグ </summary>
+    private bool IsInput = false;
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonUp(0))
+        {
+            //ラインレンダラーを初期化
+            _lineRenderer.positionCount = 0;
 
-        if (Input.GetMouseButtonDown(0))
-            SetPosition(mousePos);
-        else if (Input.GetMouseButtonUp(0))
             _posCount = 0;
+            IsInput = false;
+            Debug.Log("線を消す");
+        }
+
+        if (Input.GetMouseButtonDown(0) || IsInput)
+        {
+            IsInput = true;
+            //Rayを飛ばすスタート位置を決める
+            var rayStartPosition = _origin.transform.position;
+            //マウスでRayを飛ばす方向を決める
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Hitしたオブジェクト格納用
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                SetPosition(hit.point);
+            }  
+        }
     }
 
     /// <summary>
     /// 線を伸ばすメソッド
     /// </summary>
     /// <param name="pos">マウスの位置</param>
-    private void SetPosition(Vector2 pos)
+    private void SetPosition(Vector3 pos)
     {
         if (!PosCheck(pos))
             return;
-
+        Debug.Log("線を書いた");
         _posCount++;
         _lineRenderer.positionCount = _posCount;
         _lineRenderer.SetPosition(_posCount - 1, pos);
@@ -42,12 +63,12 @@ public class LineRendererDraw : MonoBehaviour
     /// </summary>
     /// <param name="pos">現在のマウスポジション</param>
     /// <returns>頂点を増やすかの判定</returns>
-    private bool PosCheck(Vector2 pos)
+    private bool PosCheck(Vector3 pos)
     {
         if (_posCount == 0)
             return true;
 
-        float distance = Vector2.Distance(_lineRenderer.GetPosition(_posCount - 1), pos);
+        float distance = Vector3.Distance(_lineRenderer.GetPosition(_posCount - 1), pos);
         if (distance > _interval)
             return true;
         else
