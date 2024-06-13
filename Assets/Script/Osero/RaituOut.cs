ee2733f81f890379de65e3ffb66cc0ace50d3154
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-06-13 23:56:32 +0900

    [fix]ライツアウトの修正

--- Assets/Script/Osero/RaituOut.cs
+++ Assets/Script/Osero/RaituOut.cs
@@ -3,16 +3,31 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+public enum Color
+{
+	White,
+    Black
+}
+
+public class Cell
+{
+    public int _x;
+    public int _y;
+    public Color _color;
+    public Image _imagge;
+}
 public class RaituOut : MonoBehaviour, IPointerClickHandler
 {
-    private Image[,] _cells;
+    private Cell[,] _cells;
     [SerializeField]
     private int _row = 5;
     [SerializeField]
     private int _column = 5;
+
+    private bool IsClear;
     private void Start()
     {
-        _cells = new Image[_row, _column];
+        _cells = new Cell[_row, _column];
         for (var r = 0; r < _row; r++)
         {
             for (var c = 0; c < _column; c++)
@@ -20,27 +35,45 @@ public class RaituOut : MonoBehaviour, IPointerClickHandler
                 var cell = new GameObject($"Cell({r}, {c})");
                 cell.transform.parent = transform;
                 cell.AddComponent<Image>();
-                _cells[r, c] = cell.GetComponent<Image>();
+                _cells[r, c]._x = c;
+                _cells[r, c]._y = r;
+                _cells[r, c]._color = Color.White;
+                _cells[r, c]._imagge = cell.GetComponent<Image>();
             }
         }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        if (IsClear)
+            return;
         var cell = eventData.pointerCurrentRaycast.gameObject;
         var str = cell.name;
         str.Replace("Cell{", "");
         str.Replace(")", "");
         var ints = Array.ConvertAll(str.Split(","), int.Parse);
-        int r = ints[0];
-        int c = ints[1];
+       
+        if (CheckClear())
+        {
+            IsClear = true;
+            Debug.Log("クリア");
+        }
+    }
 
-        //上下左右を変える
+    /// <summary>
+    /// クリア判定
+    /// </summary>
+    public bool CheckClear()
+    {
+        for (var r = 0; r < _row; r++)
+        {
+            for (var c = 0; c < _column; c++)
+            {
+                if (_cells[r, c]._color != Color.Black)
+                    return false;
+            }
+        }
 
-        //黒と白の色を反転させる
-        //if (image.color == Color.black)
-        //    image.color = Color.white;
-        //else
-        //    image.color = Color.black;
+        return true;
     }
 }
