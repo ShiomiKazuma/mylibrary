b3ed32140e324f4226202d9c37ab9971f19663ad
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-06-14 14:54:20 +0900

    [update]ライツアウトの更新

--- Assets/Script/Osero/RaituOut.cs
+++ Assets/Script/Osero/RaituOut.cs
@@ -2,62 +2,114 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
-public enum Color
+public enum ColorState
 {
-	White,
+    White,
     Black
-}
-
-public class Cell
-{
-    public int _x;
-    public int _y;
-    public Color _color;
-    public Image _imagge;
 }
 public class RaituOut : MonoBehaviour, IPointerClickHandler
 {
-    private Cell[,] _cells;
     [SerializeField]
     private int _row = 5;
     [SerializeField]
     private int _column = 5;
-
+    private Cell[,] _cells;
     private bool IsClear;
-    private void Start()
+
+    private void Awake()
     {
         _cells = new Cell[_row, _column];
+    }
+    private void Start()
+    {
+        var layout = GetComponent<GridLayoutGroup>();
+        layout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        layout.constraintCount = _column;
+
         for (var r = 0; r < _row; r++)
         {
             for (var c = 0; c < _column; c++)
             {
-                var cell = new GameObject($"Cell({r}, {c})");
+                GameObject cell = new GameObject($"Cell({r}, {c})");
                 cell.transform.parent = transform;
                 cell.AddComponent<Image>();
-                _cells[r, c]._x = c;
-                _cells[r, c]._y = r;
-                _cells[r, c]._color = Color.White;
-                _cells[r, c]._imagge = cell.GetComponent<Image>();
+                Debug.Log(cell.gameObject);
+                InstatnceCell(r, c, cell);
             }
         }
+        
+        InitializeCells();
     }
 
+    public void InstatnceCell(int r, int c, GameObject cell)
+    {
+        _cells[r, c]._gameObject = cell.gameObject;
+        _cells[r, c]._x = c;
+        _cells[r, c]._y = r;
+        _cells[r, c].Color = ColorState.White;
+        _cells[r, c]._image = cell.GetComponent<Image>();
+        _cells[r, c]._image.color = UnityEngine.Color.black;
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
         if (IsClear)
             return;
         var cell = eventData.pointerCurrentRaycast.gameObject;
-        var str = cell.name;
-        str.Replace("Cell{", "");
-        str.Replace(")", "");
-        var ints = Array.ConvertAll(str.Split(","), int.Parse);
-       
+        int r = 0;
+        int c = 0;
+        TryGetCell(cell, ref r, ref c);
+        CellCheck(r, c);
         if (CheckClear())
         {
             IsClear = true;
             Debug.Log("クリア");
         }
+    }
+
+    private void TryGetCell(GameObject cell, ref int row, ref int column)
+    {
+        row = 0; column = 0;
+
+        for (var r = 0; r < _cells.GetLength(0); r++)
+        {
+            for (var c = 0; c < _cells.GetLength(1); c++)
+            {
+                if (_cells[r, c]._gameObject == cell)
+                {
+                    row = r;
+                    column = c;
+                    return;
+                }
+            }
+        }
+    }
+    private void CellCheck(int r, int c)
+    {
+        //クリックされたものを判定
+        CellChange(r, c);
+        //上下左右の色を反転させる
+        if (r + 1 < _row)
+            CellChange(r + 1, c);
+        if(r - 1 >= 0)
+            CellChange(r - 1, c);
+        if(c + 1 < _column)
+            CellChange(r, c + 1);
+        if(c - 1 >= 0)
+            CellChange(r, c - 1);
+    }
+
+    /// <summary>
+    /// セルを反転させる
+    /// </summary>
+    /// <param name="r"></param>
+    /// <param name="c"></param>
+    private void CellChange(int r, int c)
+    {
+        var image = _cells[r, c]._image;
+        image.color = image.color == UnityEngine.Color.white ? UnityEngine.Color.black : UnityEngine.Color.white;
+        _cells[r, c].Color = _cells[r, c].Color == ColorState.White ? ColorState.Black : ColorState.White;
     }
 
     /// <summary>
@@ -69,11 +121,39 @@ public class RaituOut : MonoBehaviour, IPointerClickHandler
         {
             for (var c = 0; c < _column; c++)
             {
-                if (_cells[r, c]._color != Color.Black)
+                if (_cells[r, c].Color != ColorState.Black)
                     return false;
             }
         }
 
         return true;
     }
+
+    private void InitializeCells()
+    {
+        int count = 0;
+        do
+        {
+            for (var r = 0; r < _row; r++)
+            {
+                for (var c = 0; c < _column; c++)
+                {
+                    if (Random.value > 0.1) { continue; }
+
+                    // クリア済みの状態からボタンを押していく
+                    CellCheck(r, c);
+                    count++;
+                }
+            }
+        } while (CheckClear() && count >= 2);
+    }
+    public struct Cell
+    {
+        public int _x;
+        public int _y;
+        public ColorState Color;
+        public Image _image;
+        public GameObject _gameObject;
+    }
 }
+
