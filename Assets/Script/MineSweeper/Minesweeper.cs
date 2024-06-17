4d7f88af4f3996e9a17e54e853867c96268e8d53
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-06-18 00:27:27 +0900

    [update]地雷が被らないように生成する処理に更新

--- Assets/Script/MineSweeper/Minesweeper.cs
+++ Assets/Script/MineSweeper/Minesweeper.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Minesweeper : MonoBehaviour
 {
     [SerializeField] private int _row = 1;
     [SerializeField] private int _column = 1;
+    [SerializeField] private int _mineCount = 10;
     [SerializeField] private GridLayoutGroup _gridLayoutGroup = null;
     [SerializeField] private Cell _cellPrefab = null;
 
@@ -22,6 +24,21 @@ public class Minesweeper : MonoBehaviour
                 var cell = Instantiate(_cellPrefab);
                 cell.transform.SetParent(parent);
                 _cells[r, c] = cell;
+            }
+        }
+        
+        for (var i = 0; i < _mineCount; i++)
+        {
+            while (true)
+            {
+                var r = Random.Range(0, _row);
+                var c = Random.Range(0, _column);
+                if (_cells[r, c].CellState != CellState.Mine)
+                {
+                    var cell = _cells[r, c];
+                    cell.CellState = CellState.Mine;
+                    break; 
+                } //地雷が被らないようにする
             }
         }
     }
